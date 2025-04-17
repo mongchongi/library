06524fc01bbf4ef9b127e9cb5a89951bee6f0035
mongchongi <yeonguk0517@gmail.com>
2025-04-17 22:03:16 +0900

    로그인 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router';
 import BookListPage from './pages/BookListPage/BookListPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import AppLayout from './layout/AppLayout/AppLayout';
+import SignInPage from './pages/SIgnInPage/SignInPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<AppLayout />}>
         <Route index element={<BookListPage />} />
+        <Route path='/sign-in' element={<SignInPage />} />
       </Route>
 
       <Route path='*' element={<NotFoundPage />} />
